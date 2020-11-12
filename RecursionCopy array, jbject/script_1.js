a1dbe86b58516746959c44599f93da4eba2f89ba
Alex <simba-3@ukr.net>
2020-11-12 14:11:10 +0200

    Копіювання ссилочних типів даних рекурсією

--- RecursionCopy array, jbject/script_1.js
+++ RecursionCopy array, jbject/script_1.js
@@ -11,25 +11,27 @@ copyObject(someObject, copy)
 function copyObject(obj, copy) {
     function forObj(innerObj, copy) {
         for (const key in innerObj) {
-            if (!Array.isArray(innerObj[key]) && typeof innerObj[key] === 'object') {
-                forObj(innerObj[key], copy);
-            } else if (Array.isArray(innerObj[key])) {
-                const innerArr = innerObj[key];
-                copy[key] = []
-                forArr(innerObj[key], copy[key], innerArr);
-            } else {
-                copy[key] = innerObj[key];
+            if (innerObj.hasOwnProperty(key)) {
+                if (!Array.isArray(innerObj[key]) && typeof innerObj[key] === 'object') {
+                    forObj(innerObj[key], copy);
+                } else if (Array.isArray(innerObj[key])) {
+                    const innerArr = innerObj[key];
+                    copy[key] = [];
+                    forArr(innerObj[key], copy[key], innerArr);
+                } else {
+                    copy[key] = innerObj[key];
+                }
             }
         }
     }
 
-    function forArr(innerArr, copy, nextInner) {
-        for (const element of innerArr) {
+    function forArr(innerArr, copy, nextInner) { // nextInner при першому запуску не не існує, в майбутному створиться
+        for (const element of innerArr) {               // тобто якби у подальшому використання даноо параметру (nextInner)
             if (Array.isArray(element)) {
-                const nextInner = element;
-                forArr(element, copy, nextInner);
+                 nextInner = element;       // ось тут створюється nextInner
+                forArr(element, copy, nextInner); // ось тут використовується nextInner
             } else if (typeof element === 'object' && (!Array.isArray(element))) {
-                copy = []
+                copy = [];
                 forObj(element, copy,);
             } else if (typeof element !== 'object' && (!Array.isArray(element))) {
                 for (const element of nextInner) {
